9ab6fda923c267fd9853c04d3771195f8514298f
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-03-10 10:28:06 -0700

    clean up

--- src/com/miguelbarrios/util/Config.java
+++
@@ -1,9 +0,0 @@
-package com.miguelbarrios.util;
-
-public class Config {
-	private static final String authToken = "GJC3MILAXSuYMeXDXNzbYbFUPsCfsCaQHi5OuyHOTkCBDh47kQOYyI1Zpu3Y3vTF";
-
-	public static String getAuthToken() {
-		return authToken;
-	}
-}
